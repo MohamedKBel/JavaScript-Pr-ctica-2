f1284385f2327782f387c07f0ef0abdc017189ab
Mohamed KB <mohabel98@gmail,com>
2022-10-25 16:22:51 +0200

    cambios hechos

---
+++ ejerciciosJS/ejercicio6.js
@@ -0,0 +1,19 @@
+
+let nombre ="Mohamed" ;
+console.log(nombre);
+console.log(typeof(nombre));
+
+let numero = 24;
+console.log(numero);
+console.log(typeof(numero));
+
+let booleano = true;
+console.log(booleano);
+console.log(typeof(booleano));
+
+let nulo = null;
+console.log(nulo);
+console.log(typeof(nulo));
+
+let variable;
+console.log(typeof(variable));
